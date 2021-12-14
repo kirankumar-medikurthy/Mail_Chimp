9801c85fd817cc43359a9fa183719d47a1ddcc65
kirankumar-medikurthy <kiran1999m1234@gmail.com>
2021-12-14 22:40:18 +0530

    page4 work in progress

--- Mail_Chimp_Client_Side/src/components/LandingPage.jsx
+++ Mail_Chimp_Client_Side/src/components/LandingPage.jsx
@@ -1,1 +1,6 @@
-import React from "react";
+import React from 'react';
+export const LandingPage = () =>{
+    return (<div>
+        Welcome to the Landing page
+    </div>)
+}
---
+++ Mail_Chimp_Client_Side/src/components/Page4.jsx
@@ -0,0 +1,148 @@
+import "./page4.css"
+export const Page4 = () =>{
+    return (<div>
+        <h1>Welcome to the Page 4</h1>
+        <div id="navbar">
+            <div className="text1">Marketing smart for growing business</div>
+            <div className="boxes">
+                <div className="Box1">Marketing Platform</div>
+                <div className="Box2">Website & Commerce</div>
+                <div className="Box3">Transactional Email</div>
+            </div>
+            <div className="premiumboxes">
+                <div className="prembox1">
+                    <div>Premium</div>
+                    <p>Advanced features for pros who need more customization.</p>
+                    <p>Starts at</p>
+                    <div>Rs <h1 style={{display:"inline"}}>23,000</h1></div>
+                    <p>/month based on 10,000 contacts*</p>
+                    <p>How  many contacts do you have?</p>
+                    <button>Buy Now</button>
+                    <p>Everything in Standard, plus:</p>
+                    <div>
+                        <div className="insideTextpremium">
+                            <img src="/project_images/page4_images/tick.svg" alt="tick.svg"/>
+                            <div style={{paddingLeft:"10px"}}>Advanced Segmentation</div>
+                        </div>
+                        <div className="insideTextpremium">
+                            <img src="/project_images/page4_images/tick.svg" alt="tick.svg"/>
+                            <div style={{paddingLeft:"10px"}}>Multivariate Testing</div>
+                        </div>
+                        <div className="insideTextpremium">
+                            <img src="/project_images/page4_images/tick.svg" alt="tick.svg"/>
+                            <div style={{paddingLeft:"10px"}} >Comparative Reporting</div>
+                        </div>
+                        <div className="insideTextpremium">
+                            <img src="/project_images/page4_images/tick.svg" alt="tick.svg"/>
+                            <div style={{paddingLeft:"10px"}}>Unlimited seats & Role-Based Access</div>
+                        </div>
+                        <div className="insideTextpremium">
+                            <img src="/project_images/page4_images/tick.svg" alt="tick.svg"/>
+                            <div style={{paddingLeft:"10px"}} >Phone Support</div>
+                        </div>
+                    </div>
+                    <div>About Premium</div>
+                </div>
+                <div className="prembox2">
+                    <div>Standard</div>
+                    <p>Data-driven automation and optimization faster.</p>
+                    <p>Starts at</p>
+                    <div>Rs <h1 style={{display:"inline"}}>1,150</h1></div>
+                    <p>/month based on 10,000 contacts*</p>
+                    <p>How  many contacts do you have?</p>
+                    <button>Buy Now</button>
+                    <p>Everything in Standard, plus:</p>
+                    <div>
+                        <div className="insideTextpremium">
+                            <img src="/project_images/page4_images/tick.svg" alt="tick.svg"/>
+                            <div style={{paddingLeft:"10px"}}>Customer Journey Builder+ Branching Points</div>
+                        </div>
+                        <div className="insideTextpremium">
+                            <img src="/project_images/page4_images/tick.svg" alt="tick.svg"/>
+                            <div style={{paddingLeft:"10px"}}>Send Time Optimization</div>
+                        </div>
+                        <div className="insideTextpremium">
+                            <img src="/project_images/page4_images/tick.svg" alt="tick.svg"/>
+                            <div style={{paddingLeft:"10px"}} >Behavioral Targeting</div>
+                        </div>
+                        <div className="insideTextpremium">
+                            <img src="/project_images/page4_images/tick.svg" alt="tick.svg"/>
+                            <div style={{paddingLeft:"10px"}}>Custom Templates</div>
+                        </div>
+                        <div className="insideTextpremium">
+                            <img src="/project_images/page4_images/tick.svg" alt="tick.svg"/>
+                            <div style={{paddingLeft:"10px"}} >Dynamic Content</div>
+                        </div>
+                    </div>
+                    <div>About Premium</div>
+                </div>
+                <div className="prembox3">
+                    <div>Essentials</div>
+                    <p>Great for email-only senders who want around-the-clock support.</p>
+                    <p>Starts at</p>
+                    <div>Rs <h1 style={{display:"inline"}}>770</h1></div>
+                    <p>/month based on 10,000 contacts*</p>
+                    <p>How  many contacts do you have?</p>
+                    <button>Buy Now</button>
+                    <p>Everything in Standard, plus:</p>
+                    <div>
+                        <div className="insideTextpremium">
+                            <img src="/project_images/page4_images/tick.svg" alt="tick.svg"/>
+                            <div style={{paddingLeft:"10px"}}>Email Templates</div>
+                        </div>
+                        <div className="insideTextpremium">
+                            <img src="/project_images/page4_images/tick.svg" alt="tick.svg"/>
+                            <div style={{paddingLeft:"10px"}}>Multi-Step Journey</div>
+                        </div>
+                        <div className="insideTextpremium">
+                            <img src="/project_images/page4_images/tick.svg" alt="tick.svg"/>
+                            <div style={{paddingLeft:"10px"}} >Custom Branding</div>
+                        </div>
+                        <div className="insideTextpremium">
+                            <img src="/project_images/page4_images/tick.svg" alt="tick.svg"/>
+                            <div style={{paddingLeft:"10px"}}>A/B Testing</div>
+                        </div>
+                        <div className="insideTextpremium">
+                            <img src="/project_images/page4_images/tick.svg" alt="tick.svg"/>
+                            <div style={{paddingLeft:"10px"}} >24/7 Email & Chat Support</div>
+                        </div>
+                    </div>
+                    <div>About Premium</div>
+                </div>
+                <div className="prembox4">
+                <div>Free</div>
+                    <p>All the multi-channel tools you need to audience.</p>
+                    <p>Starts at</p>
+                    <div>Rs <h1 style={{display:"inline"}}>0</h1></div>
+                    <p>/month based on 10,000 contacts*</p>
+                    <p>How  many contacts do you have?</p>
+                    <button>Buy Now</button>
+                    <p>Everything in Standard, plus:</p>
+                    <div>
+                        <div className="insideTextpremium">
+                            <img src="/project_images/page4_images/tick.svg" alt="tick.svg"/>
+                            <div style={{paddingLeft:"10px"}}>Marketing CRM</div>
+                        </div>
+                        <div className="insideTextpremium">
+                            <img src="/project_images/page4_images/tick.svg" alt="tick.svg"/>
+                            <div style={{paddingLeft:"10px"}}>Creative Assistant</div>
+                        </div>
+                        <div className="insideTextpremium">
+                            <img src="/project_images/page4_images/tick.svg" alt="tick.svg"/>
+                            <div style={{paddingLeft:"10px"}} >Website Builder</div>
+                        </div>
+                        <div className="insideTextpremium">
+                            <img src="/project_images/page4_images/tick.svg" alt="tick.svg"/>
+                            <div style={{paddingLeft:"10px"}}>Mailchimp Domain</div>
+                        </div>
+                        <div className="insideTextpremium">
+                            <img src="/project_images/page4_images/tick.svg" alt="tick.svg"/>
+                            <div style={{paddingLeft:"10px"}} >Forms & Landing Pages</div>
+                        </div>
+                    </div>
+                    <div>About Premium</div>
+                </div>
+            </div>
+        </div>
+    </div>)
+}
