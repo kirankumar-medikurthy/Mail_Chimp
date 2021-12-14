678cfbb5337fe492c38b8964fc07a39b63976767
kirankumar-medikurthy <kiran1999m1234@gmail.com>
2021-12-14 22:42:03 +0530

    page4 demo is seen

---
+++ Mail_Chimp_Client_Side/src/App.jsx
@@ -0,0 +1,12 @@
+import { Page4 } from './components/Page4';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Page4 />
+    </div>
+  );
+}
+
+export default App;
